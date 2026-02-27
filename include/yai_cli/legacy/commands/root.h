37323acab86a5750ecadf1962eb65bcced458f38
Francesco Maiomascio <francesco@maiomascio.dev>
2026-02-27 20:15:41 +0100

    chore(cli): remove legacy command headers

--- include/yai_cli/legacy/commands/root.h
+++
@@ -1,8 +0,0 @@
-#ifndef YAI_CLI_COMMANDS_ROOT_H
-#define YAI_CLI_COMMANDS_ROOT_H
-
-#include <yai_cli/cli.h>
-
-int yai_cmd_root(int argc, char **argv, const yai_cli_opts_t *opt);
-
-#endif
--- include/yai_cli/legacy/commands/workspace.h
+++
@@ -1,8 +0,0 @@
-#ifndef YAI_CLI_COMMANDS_WORKSPACE_H
-#define YAI_CLI_COMMANDS_WORKSPACE_H
-
-#include <yai_cli/cli.h>
-
-int yai_cmd_ws(int argc, char **argv, const yai_cli_opts_t *opt);
-
-#endif
